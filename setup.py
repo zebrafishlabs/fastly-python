d089f28fb0eaa28dae9c50a4f3522c7f47210283
Chris Zacharias <christopher.zacharias@gmail.com>
2012-03-21 21:33:03 -0700

    Adding a setup.py file.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name = "fastly-python",
+	version = "0.0.1",
+	author = "Chris Zacharias",
+	author_email = "chris@imgix.com",
+	description = ("A Python client libary for the Fastly API."),
+	license = "BSD",
+	keywords = "fastly",
+	url = "https://github.com/zebrafishlabs/fastly-python",
+	packages=['fastly', 'tests'],
+	long_description=read('README'),
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"License :: OSI Approved :: BSD License",
+	],
+)
